c0be0ce6e98032f8ff19c3fb8d0a28faa1f56a9b
aruay99 <aruaiberdk@gmail.com>
2025-04-15 21:32:36 +0500

    fix: added a line yam likes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const app = new Elysia()
   .use(setupApp)
   .get('/', ({ redirect }) => redirect('/swagger'))
   .listen(3000);
+  
 console.log(
   `🦊 Elysia is running! Access Swagger UI at http://${app.server?.hostname}:${app.server?.port}/swagger`,
 );
