59bd5a7b6683cebde9d6e85589c1ba324128efc7
Yam Borodetsky <yam@yam.codes>
2025-06-08 00:57:52 +0500

    refactor: update articles schema to include comments import
    - Added import for comments schema to the articles schema file to enhance data relationships and maintainability.
    - Removed the previous relative import of comments schema for consistency with the new absolute import structure.

--- src/articles/articles.schema.ts
+++ src/articles/articles.schema.ts
@@ -1,4 +1,5 @@
 import { users } from '@/users/users.model';
+import { comments } from '@comments/schema';
 import { articleTags } from '@tags/tags.model';
 import { relations } from 'drizzle-orm';
 import {
@@ -9,7 +10,6 @@ import {
   text,
   timestamp,
 } from 'drizzle-orm/pg-core';
-import { comments } from '../comments/schema';
 
 export const articles = table('articles', {
   id: serial('id').primaryKey(),
