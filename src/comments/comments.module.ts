ca0911707e97d4311e30a4e75370cd2d7217e5d0
Yam Borodetsky <yam@yam.codes>
2025-06-09 00:25:35 +0500

    refactor: enhance comments module with article and tag services integration
    - Integrated ArticlesService and TagsService into CommentsService for improved comment handling related to articles and tags.
    - Removed the deprecated findBySlug method from CommentsRepository, streamlining the repository's functionality.
    - Updated setupComments function to instantiate and utilize the new services, enhancing modularity and maintainability.

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,17 +1,30 @@
 import { db } from '@/database.providers';
+import { ArticlesRepository } from '@articles/articles.repository';
+import { ArticlesService } from '@articles/articles.service';
 import { AuthService } from '@auth/auth.service';
 import { ProfilesRepository } from '@profiles/profiles.repository';
 import { ProfilesService } from '@profiles/profiles.service';
+import { TagsRepository } from '@tags/tags.repository';
+import { TagsService } from '@tags/tags.service';
 import { Elysia } from 'elysia';
 import { CommentsRepository } from './comments.repository';
 import { CommentsService } from './comments.service';
 
 export const setupComments = () => {
   const commentsRepository = new CommentsRepository(db);
+  const articlesRepository = new ArticlesRepository(db);
   const profilesRepository = new ProfilesRepository(db);
+  const tagsRepository = new TagsRepository(db);
   const profilesService = new ProfilesService(profilesRepository);
+  const tagsService = new TagsService(tagsRepository);
+  const articlesService = new ArticlesService(
+    articlesRepository,
+    profilesService,
+    tagsService,
+  );
   const commentsService = new CommentsService(
     commentsRepository,
+    articlesService,
     profilesService,
   );
   const authService = new AuthService();
